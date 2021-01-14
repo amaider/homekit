0752bf21a4f1de62c0fb446051d80175bcf1b0dc
67393022 <amaider@web.de>
2021-01-14 16:57:30 +0100

    add bme280

---
+++ amaider_examples/bme280/bme280.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include "../../esp-homekit-demo/wifi.h"
+
+#define LED_INBUILT_GPIO 2
+
+#include "espressif/esp_common.h"   //do i need this?
+
+#include "i2c/i2c.h"
+#include "bmp280/bmp280.h"
+
+const uint8_t i2c_bus = 0;
+const uint8_t scl_pin = 5;
+const uint8_t sda_pin = 4;
+
+float temperature_value, pressure_value, humidity_value;
+
+homekit_characteristic_t temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+homekit_characteristic_t humidity    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+void bmp280_sensor_task(void *pvParameters) {
+    bmp280_params_t  params;
+
+    bmp280_init_default_params(&params);
+
+    bmp280_t bmp280_dev;
+    bmp280_dev.i2c_dev.bus = i2c_bus;
+    bmp280_dev.i2c_dev.addr = BMP280_I2C_ADDRESS_0;
+
+    while (1) {
+        while (!bmp280_init(&bmp280_dev, &params)) {
+            printf("BMP280 initialization failed\n");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+        while(1) {
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            if (!bmp280_read_float(&bmp280_dev, &temperature_value, &pressure_value, &humidity_value)) {
+                printf("Temperature/pressure reading failed\n");
+                break;
+            }
+            //printf("Humidity: %.2f Pa, Temperature: %.2f C\n", humidity_value, temperature_value);
+            temperature.value.float_value = temperature_value;
+            humidity.value.float_value = humidity_value;
+            homekit_characteristic_notify(&temperature, HOMEKIT_FLOAT(temperature_value));
+            homekit_characteristic_notify(&humidity, HOMEKIT_FLOAT(humidity_value));
+            vTaskDelay(3000 / portTICK_PERIOD_MS);
+        }
+    }
+}
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_thermostat, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "amaider"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0012345"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Temperature&Humidity"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, NULL),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            &temperature,
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &humidity,
+            NULL
+        }),
+    }),
+    NULL
+};
+
+static void wifi_init() {
+    struct sdk_station_config wifi_config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASSWORD,
+    };
+
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&wifi_config);
+    sdk_wifi_station_connect();
+}
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+};
+
+void user_init(void) {
+    wifi_init();
+    homekit_server_init(&config);
+    i2c_init(i2c_bus, scl_pin, sda_pin, I2C_FREQ_400K);
+    xTaskCreate(bmp280_sensor_task, "Temperatore Sensor", 256, NULL, 2, NULL);
+}
